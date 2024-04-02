bc4bb1c354c28ab3d9aa1afcef11e22645f742d7
Can Cömertpay <cancomertpay@gmail.com>
2024-04-02 17:57:17 +0300

    Fix body element's and planets rotation overflow bug

--- components/root-layout/main-header.js
+++ components/root-layout/main-header.js
@@ -30,64 +30,69 @@ function MainHeader() {
   };
 
   return (
-    <motion.header
-      initial={{ y: -60, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ delay: 0.9, type: "tween" }}
-      className="z-10 w-full fixed top-0 lg:top-10 flex items-center justify-between p-5 md:p-0 bg-primary-black/40 backdrop-blur-sm "
-    >
-      {/* brand logo */}
-      <motion.div
-        whileTap={{
-          scale: 0.8,
-        }}
-        className="md:mx-10 flex items-center justify-center"
+    <>
+      <motion.header
+        initial={{ y: -60, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.9, type: "tween" }}
+        className="z-10 w-full fixed md:top-0 top-0 lg:top-10 flex items-center justify-between p-5 md:p-0 bg-primary-black/40 backdrop-blur-sm "
       >
-        <Link href="/">
-          <Image
-            src={brandLogo}
-            alt="Space tourism's brand logo."
-            width="42"
-            height="42"
-            className="md:w-[52px] md:h-[52px]"
-          />
-        </Link>
-      </motion.div>
-      {/* underline for only lg devices */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.6 }}
-        className="hidden lg:block border-b-2 border-pale-blue/20 flex-1"
-      />
-      {/* hamburger icon for only sm devices */}
-      {!isAsideOpen && (
+        {/* brand logo */}
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
           whileTap={{
             scale: 0.8,
           }}
-          exit={{ opacity: 0 }}
-          transition={{ delay: 0.4 }}
-          onClick={openAsideBar}
-          className="md:hidden"
+          className="md:mx-10 flex items-center justify-center"
         >
-          <Image
-            src={hamburgerIcon}
-            alt="An icon for to opening aside navigation bar."
-            width="32px"
-            height="32px"
-          />
+          <Link href="/">
+            <Image
+              src={brandLogo}
+              alt="Space tourism's brand logo."
+              width="42"
+              height="42"
+              className="md:w-[52px] md:h-[52px]"
+            />
+          </Link>
         </motion.div>
-      )}
-      {/* main navigation bar for md devices */}
-      <MainNavbar />
-      {/* aside navigation bar for only sm devices */}
-      <AnimatePresence>
-        {isAsideOpen && <AsideNavigationBar onClose={closeAsideBar} />}
-      </AnimatePresence>
-    </motion.header>
+        {/* underline for only lg devices */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.6 }}
+          className="hidden lg:block border-b-2 border-pale-blue/20 flex-1"
+        />
+        {/* hamburger icon for only sm devices */}
+        {!isAsideOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileTap={{
+              scale: 0.8,
+            }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 0.4 }}
+            onClick={openAsideBar}
+            className="md:hidden"
+          >
+            <Image
+              src={hamburgerIcon}
+              alt="An icon for to opening aside navigation bar."
+              width="32px"
+              height="32px"
+            />
+          </motion.div>
+        )}
+        {/* main navigation bar for md devices */}
+        <MainNavbar />
+        {/* aside navigation bar for only sm devices */}
+        <AnimatePresence>
+          {isAsideOpen && <AsideNavigationBar onClose={closeAsideBar} />}
+        </AnimatePresence>
+      </motion.header>
+      <div className="hidden lg:flex absolute right-0 text-white text-xl">
+        {">"}
+      </div>
+    </>
   );
 }
 
